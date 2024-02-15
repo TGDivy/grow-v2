90d6da472915ccd0da0f6d8177ce30f031e6ae41
tgdivy <me@divyb.xyz>
2024-02-15 06:43:54 +0000

    [FEAT]: Hello world server!

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,7 @@ import todoRoutes from "./routes/todos";
 import { json, urlencoded } from "body-parser";
 import admin from "firebase-admin";
 import serviceAccount from "./serviceAccountKey.json";
+import { PORT } from "./constants";
 
 const app = express();
 
@@ -11,34 +12,15 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
-app.post("/signup", async (req, res) => {
-  console.log("Request", req.body);
-  try {
-    const { email, password } = req.body;
-    const user = await admin.auth().createUser({
-      email,
-      password,
-      emailVerified: false,
-      disabled: false,
-    });
-    res.status(200).json(user);
-  } catch (error) {
-    if (!(error instanceof Error)) {
-      res.status(500).json({ message: "Internal server error" });
-      return;
-    }
-    res.status(500).json({ message: error.message });
-  }
-});
-
 app.use(json());
-
 app.use(urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 8080;
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}, http://localhost:${PORT}`);
 });
 
 // app.use("/todos", todoRoutes);
