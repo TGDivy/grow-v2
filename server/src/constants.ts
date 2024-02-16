57d0aa26fcf9d37ed58707d035d87ee857920b98
tgdivy <me@divyb.xyz>
2024-02-16 00:15:08 +0000

    log

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -15,5 +15,7 @@ export const FIREBASE_CONFIG: admin.ServiceAccount = {
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
+console.log(FIREBASE_CONFIG);
+
 export const PORT = Number(process.env.PORT) || 8080;
 export const DB_CONN_STRING = process.env.DB_CONN_STRING;
