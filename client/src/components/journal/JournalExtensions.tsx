135a014a46e6f22b6933f80427f229e4bb869edc
tgdivy <me@divyb.xyz>
2024-03-26 05:31:09 +0300

    [FEAT]: Input for journal

---
+++ client/src/components/journal/JournalExtensions.tsx
@@ -0,0 +1,57 @@
+import { Extensions } from "@tiptap/react";
+import { dueDateConfig } from "../rte/DueDatePlugin/due_date.config.";
+import { DueDate } from "../rte/DueDatePlugin/due_date.plugin";
+import { Mention } from "../rte/mention";
+import { projectsConfig } from "../rte/ProjectPlugin/projects.config";
+
+import Block from "@tiptap/extension-blockquote";
+import BulletList from "@tiptap/extension-bullet-list";
+import Document from "@tiptap/extension-document";
+import HardBreak from "@tiptap/extension-hard-break";
+import Heading from "@tiptap/extension-heading";
+import HorizontalRule from "@tiptap/extension-horizontal-rule";
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+
+import { Bold } from "@tiptap/extension-bold";
+import { Code } from "@tiptap/extension-code";
+import { Italic } from "@tiptap/extension-italic";
+import { Strike } from "@tiptap/extension-strike";
+
+import { Dropcursor } from "@tiptap/extension-dropcursor";
+import { Gapcursor } from "@tiptap/extension-gapcursor";
+import { History } from "@tiptap/extension-history";
+
+import CharacterCount from "@tiptap/extension-character-count";
+import { Placeholder } from "@tiptap/extension-placeholder";
+
+export const journalExtensions: Extensions = [
+  Block,
+  BulletList,
+  Document,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  ListItem,
+  OrderedList,
+  Paragraph,
+  Text,
+
+  Bold,
+  Code,
+  Italic,
+  Strike,
+
+  Dropcursor,
+  Gapcursor,
+  History,
+
+  Mention.configure(projectsConfig),
+  DueDate.configure(dueDateConfig),
+  CharacterCount.configure({}),
+  Placeholder.configure({
+    placeholder: "Enter your thoughts here …",
+  }),
+];
--- client/src/pages/Journal/JournalSessionPage.tsx
+++ client/src/pages/Journal/JournalSessionPage.tsx
@@ -1,6 +1,85 @@
-import { Row, Col, Card, Empty, Typography } from "antd";
+import { Editor, EditorContent, FloatingMenu, useEditor } from "@tiptap/react";
+import { Button, Col, Row, Typography } from "antd";
+import dayjs from "dayjs";
+import { journalExtensions } from "src/components/journal/JournalExtensions";
+
+// Default template based on advice from great leaders about journalling
+// in basic html format (no headings, use blockquote if needed)
+// const defaultTemplates = [
+//   `<p>Today, I am thankful for …</p>
+//   <p></p>
+//   <p>What would make today great?</p>
+//   <p>Daily affirmations. I am …</p>
+//   <p>3 amazing things that happened today …</p>
+//   `,
+// ];
+
+const FloatingMenuComponent = ({ editor }: { editor: Editor }) => {
+  return (
+    <FloatingMenu
+      editor={editor}
+      tippyOptions={{
+        duration: 100,
+        offset: [0, 40],
+      }}
+    >
+      <Button.Group>
+        <Button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+          }
+          size="small"
+        >
+          h1
+        </Button>
+        <Button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+          }
+          size="small"
+        >
+          h2
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+          size="small"
+        >
+          bullet list
+        </Button>
+        {/* quote */}
+        <Button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive("blockquote") ? "is-active" : ""}
+          size="small"
+        >
+          quote
+        </Button>
+      </Button.Group>
+    </FloatingMenu>
+  );
+};
 
 const JournalSessionPage = () => {
+  const editor = useEditor({
+    extensions: journalExtensions,
+    editorProps: {
+      attributes: {
+        class: "tiptapJournal",
+      },
+    },
+  });
+
+  if (!editor) {
+    return null;
+  }
+
   return (
     <>
       <div
@@ -12,35 +91,80 @@ const JournalSessionPage = () => {
         }}
       >
         <Row
-          gutter={[16, 32]}
+          gutter={[16, 16]}
           style={{
             maxWidth: "850px",
             width: "100%",
             height: "100%",
           }}
         >
-          <Col
-            md={24}
-            style={{
-              textAlign: "center",
-            }}
-          >
-            <Card bordered={false}>
-              <Empty
-                image={Empty.PRESENTED_IMAGE_SIMPLE}
-                description={"Work in Progress"}
+          <Col xs={24}>
+            {/* Day in words \n Day in number \n Month in words  */}
+            <div
+              style={{
+                textAlign: "center",
+                width: "max-content",
+              }}
+            >
+              <Typography.Text
+                style={{
+                  textTransform: "uppercase",
+                }}
               >
-                <Typography.Title level={5} disabled>
-                  Settings Feature Coming Soon
-                </Typography.Title>
-                <Typography.Paragraph disabled>
-                  We are working extremely hard to bring you this feature.
-                  Please check back later. This will be the place to manage your
-                  account settings.
-                </Typography.Paragraph>
-              </Empty>
-            </Card>
+                {dayjs().format("dddd")}
+              </Typography.Text>
+              <Typography.Title level={1}>
+                {dayjs().format("DD")}
+              </Typography.Title>
+              <Typography.Text
+                style={{
+                  textTransform: "uppercase",
+                }}
+              >
+                {dayjs().format("MMMM")}
+              </Typography.Text>
+            </div>
           </Col>
+          <Col xs={24}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                height: "400px",
+                overflow: "clip",
+                position: "relative",
+              }}
+            >
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  overflow: "auto",
+                }}
+              >
+                <EditorContent editor={editor} />
+              </div>
+              <FloatingMenuComponent editor={editor} />
+              <Typography.Text
+                style={{
+                  textAlign: "end",
+                }}
+              >
+                {editor.storage.characterCount.words()} words
+              </Typography.Text>
+            </div>
+          </Col>
+          {/* <Col md={24}>
+            <Flex gap={16} align="middle" justify="center">
+              <Typography.Title level={2}>
+                <MehOutlined />
+              </Typography.Title>
+
+              <div style={{ flex: 1 }}>
+                <Slider min={0} max={10} dots step={1} defaultValue={0} />
+              </div>
+            </Flex>
+          </Col> */}
         </Row>
       </div>
     </>
